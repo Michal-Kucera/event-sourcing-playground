32871174fe8ccfa06908902025b6e9c3145a76e7
Michal Kucera <michal@finmid.com>
2025-03-01 12:04:30 +0100

    ✨ Add EventSourcingApplication

---
+++ src/test/kotlin/com/michal/EventSourcingApplicationTest.kt
@@ -0,0 +1,12 @@
+package com.michal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventSourcingApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+}
