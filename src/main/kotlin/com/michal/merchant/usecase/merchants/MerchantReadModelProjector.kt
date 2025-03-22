9e56e49fcb4e68f914826ca497e6f0bdfe545971
Michal Kucera <michal@finmid.com>
2025-03-22 14:38:10 +0100

    ✨ Show all events in projector

--- src/main/kotlin/com/michal/merchant/usecase/merchants/MerchantReadModelProjector.kt
+++ src/main/kotlin/com/michal/merchant/usecase/merchants/MerchantReadModelProjector.kt
@@ -1,6 +1,9 @@
 package com.michal.merchant.usecase.merchants
 
 import com.michal.merchant.domain.event.MerchantEvent
+import com.michal.merchant.domain.event.MerchantEvent.MerchantOnboarded
+import com.michal.merchant.domain.event.MerchantEvent.PiiDataReconciled
+import com.michal.merchant.domain.event.MerchantEvent.PiiDataSubmitted
 import org.axonframework.config.ProcessingGroup
 import org.axonframework.eventhandling.EventHandler
 import org.springframework.stereotype.Component
@@ -13,6 +16,19 @@ class MerchantReadModelProjector {
     @EventHandler
     fun on(event: MerchantEvent) {
         println("Applying ${event.javaClass.simpleName} to merchant ${event.aggregateId} projection")
+        when (event) {
+            is MerchantOnboarded -> {
+                // TODO: do something
+            }
+
+            is PiiDataReconciled -> {
+                // TODO: do something
+            }
+
+            is PiiDataSubmitted -> {
+                // TODO: do something
+            }
+        }
         if (Random.nextBoolean()) {
             error("Merchant ${event.aggregateId} is not good!")
         }
