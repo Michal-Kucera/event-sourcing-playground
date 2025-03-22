3fe601570c4e131e9348ce844c0436653481759f
Michal Kucera <michal@finmid.com>
2025-03-22 16:47:18 +0100

    ✨ Add live projection for a specific version of PII data

--- src/main/kotlin/com/michal/merchant/usecase/merchants/MerchantsResource.kt
+++ src/main/kotlin/com/michal/merchant/usecase/merchants/MerchantsResource.kt
@@ -2,7 +2,10 @@ package com.michal.merchant.usecase.merchants
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.michal.jooq.public.tables.references.MERCHANT_PROJECTION
+import com.michal.merchant.domain.event.MerchantEvent
+import com.michal.merchant.domain.valueobject.PiiData.Version
 import com.michal.merchant.usecase.merchants.MerchantReadModelProjector.Projection
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine
 import org.jooq.DSLContext
 import org.springframework.http.HttpStatus.OK
 import org.springframework.http.ResponseEntity.notFound
@@ -17,6 +20,7 @@ import java.util.UUID
 class MerchantsResource(
     private val jooqContext: DSLContext,
     private val objectMapper: ObjectMapper,
+    private val eventStorageEngine: EventStorageEngine
 ) {
 
     @GetMapping("/merchants")
@@ -36,4 +40,42 @@ class MerchantsResource(
             }.body(projection)
         }
         ?: notFound().build()
+
+    @GetMapping("/merchants/{merchant-id}/pii-data/{version}")
+    fun findSpecificPiiDataVersionForMerchant(
+        @PathVariable("merchant-id") merchantId: UUID,
+        @PathVariable("version") version: Int
+    ) = eventStorageEngine.readEvents(merchantId.toString())
+        .asSequence()
+        .firstOrNull { (it.payload as? MerchantEvent.PiiDataSubmitted)?.version == Version.of(version) }
+        ?.let { it.payload as MerchantEvent.PiiDataSubmitted }
+        ?.let {
+            PiiDataSubmission(
+                merchantId = merchantId,
+                version = version,
+                name = it.name.value,
+                vatNumber = it.legalEntityId.vatNumber,
+                registrationNumber = it.legalEntityId.registrationNumber,
+                countryCode = it.legalAddress.country.code,
+                postCode = it.legalAddress.postCode,
+                city = it.legalAddress.city,
+                addressLine1 = it.legalAddress.addressLine1,
+                addressLine2 = it.legalAddress.addressLine2,
+            )
+        }
+        ?.let { ok(it) }
+        ?: notFound().build()
+
+    data class PiiDataSubmission(
+        val merchantId: UUID,
+        val version: Int,
+        val name: String,
+        val vatNumber: String?,
+        val registrationNumber: String?,
+        val countryCode: String,
+        val postCode: String,
+        val city: String,
+        val addressLine1: String,
+        val addressLine2: String?,
+    )
 }
